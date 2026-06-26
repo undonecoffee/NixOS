8623a1afa78426762afd2cef3899837aa607cbf4
undonecoffee <cameronwilcox101@gmail.com>
2026-06-26 17:50:42 -0500

    here

--- config.nix
+++ config.nix
@@ -3,24 +3,22 @@
   pkgs,
   ...
 }: {
-
-  programs.bash.promptInit = ''export PS1=" \[\e[92m\]nix \[\e[96m\]❯\[\e[0m\] " '';
+  programs.bash.promptInit = ''export PS1=" \[\e[92m\]nix \[\e[96m\]❯❯\[\e[0m\] " '';
   environment.shellAliases = {
     cn = "sudo vim /etc/nixos/configuration.nix";
     rb = "sudo nixos-rebuild switch";
     rbw = "/srv/cameronwilcox/deploy.sh";
     vdb = "sqlite3 -column -header data/camcam.db 'SELECT * FROM entries;'";
   };
-security.sudo.extraRules = [
-  {
-    users = [ "undonecoffee" ];
-    commands = [
-      {
-        command = "/run/current-system/sw/bin/systemctl restart camcam";
-        options = [ "NOPASSWD" ];
-      }
-    ];
-  }
-];
-
+  security.sudo.extraRules = [
+    {
+      users = ["undonecoffee"];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/systemctl restart camcam";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
 }
